103bf60a919e88ff06b4c46b4f20b3db82f75081
CJ Avilla <cjavilla@gmail.com>
2014-04-23 16:02:19 -0700

    adding some specs for starting out the sec_4_parser

--- lib/sec_query/parsers/sec_4/sec_4_parser.rb
+++ lib/sec_query/parsers/sec_4/sec_4_parser.rb
@@ -11,20 +11,24 @@ class Sec4Parser
   end
 
   def filing_xml
-    Nokogiri::XML(content).xpath('//ownershipDocument').to_xml
+    ownership_document.to_xml
   end
 
   private
+
+  def ownership_document
+    Nokogiri::XML(content).xpath('//ownershipDocument')
+  end
 
   def schema
     @schema ||= Nokogiri::XML::Schema(IO.read(xsd_uri))
   end
 
-  def xml_errors
-    @errors ||= schema.validate(filing_xml)
-  end
-
   def valid?
     xml_errors.empty?
+  end
+
+  def xml_errors
+    @errors ||= schema.validate(filing_xml)
   end
 end
--- spec/sec_query/parsers/sec_4/sec_4_parser_spec.rb
+++ spec/sec_query/parsers/sec_4/sec_4_parser_spec.rb
@@ -2,19 +2,39 @@ require 'spec_helper'
 
 describe Sec4Parser do
   let(:filing) { double('filing') }
+  let(:filing1txt) do
+    File.read(File.expand_path(File.dirname(__FILE__) + '/filing1.txt'))
+  end
 
-  before(:each) do
-    allow(filing).to receive(:content).and_return("Filing Content")
+  let(:filing1xml) do
+    Nokogiri::XML(
+      File.read(
+        File.expand_path(File.dirname(__FILE__) + '/filing1.xml')
+      )
+    ).xpath('//ownershipDocument').to_xml
   end
 
   it '#initialize takes the filing and exposes its content' do
+    allow(filing).to receive(:content).and_return("Filing Content")
     parser = Sec4Parser.new(filing)
     expect(parser.content).to eq("Filing Content")
   end
 
-  it '#parse returns a populated document containing methods for form4' do
+  it '#filing_xml returns just the xml portion of the filing content' do
+    allow(filing).to receive(:content).and_return(filing1txt)
     parser = Sec4Parser.new(filing)
-    doc = parser.parse
-    expect(doc).to respond_to(:schema_version)
+    expect(parser.content).to eq(filing1txt)
+    expect(parser.filing_xml).to eq(filing1xml)
   end
+
+#   # describe '#parse' do
+#   #   let(:doc) do
+#   #     allow(filing).to receive(:content).and_return(filing1txt)
+#   #     Sec4Parser.new(filing).parse
+#   #   end
+#
+#   #   it 'parses the schemaVersion' do
+#   #     expect(doc.schema_version).to eq('X0306')
+#   #   end
+#   # end
 end
